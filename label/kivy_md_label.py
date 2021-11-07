a33dd153fb369540a8393282e702dd3964915a70
Gowrishankar P <atmegarobotics@gmail.com>
2021-11-07 09:30:51 +0530

    hello world label in kivymd

--- label/kivy_md_label.py
+++ label/kivy_md_label.py
@@ -1,2 +1,9 @@
 from kivymd.app import MDApp
-from kiv
+from kivymd.font_definitions import theme_font_styles
+from kivymd.uix.label import MDLabel
+
+class demo(MDApp):
+    def build(self):
+        return MDLabel(text="Hello world", halign ="center", font_style = "Caption",pos=(0,100));
+Demo = demo();
+Demo.run();
