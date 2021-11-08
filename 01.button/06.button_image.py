cecf56de77f3bbe52f3de43e80829e4182dfa739
Gowrishankar P <atmegarobotics@gmail.com>
2021-11-08 15:13:21 +0530

    button_image

--- 01.button/06.button_image.py
+++ 01.button/06.button_image.py
@@ -1,6 +1,5 @@
 from kivymd.app import MDApp 
 from kivy.uix.button import Button 
-
 class demo(MDApp):
     def build(self):
         but = Button(background_normal = '/home/atmegarobotics/Desktop/kivy/3.jpg',size =(10 , 10) , size_hint=(.1 , .1), pos =(350, 250) );
