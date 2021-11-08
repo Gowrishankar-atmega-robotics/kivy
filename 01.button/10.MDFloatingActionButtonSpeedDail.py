d4bfad1e23da4aa80d5b7e7843ffee7878c70ca0
Gowrishankar P <atmegarobotics@gmail.com>
2021-11-08 17:17:15 +0530

    floating button

---
+++ 01.button/10.MDFloatingActionButtonSpeedDail.py
@@ -0,0 +1,21 @@
+import os
+os.environ["KIVY_TEXT"] = "pil"
+
+from kivy.lang import Builder 
+from kivymd.app import MDApp 
+
+kv = '''
+MDScreen:
+    MDFloatingActionButtonSpeedDial:
+        data : app.data 
+        root_button_anim: True
+'''
+class demo(MDApp):
+    data = {
+        'Python': 'language-python',
+        'PHP' : 'language-php'
+    }
+    def build(self):
+        build = Builder.load_string(kv);
+        return build 
+demo().run();
