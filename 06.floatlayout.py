5966c0adca5f53aa48233440a22997bef32fea7a
Gowrishankar P <atmegarobotics@gmail.com>
2021-11-09 07:25:49 +0530

    floatlayout

---
+++ 06.floatlayout.py
@@ -0,0 +1,11 @@
+from kivy.app import App 
+from kivy.uix.floatlayout import FloatLayout 
+from kivy.uix.button import Button 
+
+class demo(App):
+    def build(self):
+        lay = FloatLayout();
+        bt = Button(text="click here", size_hint=(.1 , .1),pos=(300,300))
+        lay.add_widget(bt)
+        return lay
+demo().run();
