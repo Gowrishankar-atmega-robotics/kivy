fbd3fd9fb4ce532dc40a502d08c24e2e452a4ce0
Gowrishankar P <atmegarobotics@gmail.com>
2021-11-09 07:34:16 +0530

    floatlayout

--- 03.Layouts/06.floatlayout.py
+++ 03.Layouts/06.floatlayout.py
@@ -5,7 +5,7 @@ from kivy.uix.button import Button
 class demo(App):
     def build(self):
         lay = FloatLayout();
-        bt = Button(text="click here", size_hint=(.1 , .1),pos=(300,300))
-        lay.add_widget(bt)
+        but = Button(text="click here", size_hint=(.1 , .1),pos=(300,300))
+        lay.add_widget(but)
         return lay
 demo().run();
