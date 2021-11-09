0684fde8fcb19e650800b19ef2ac897a6dbca8e7
Gowrishankar P <atmegarobotics@gmail.com>
2021-11-09 15:22:45 +0530

    screen

--- 04.Screen/01.Screen.py
+++ 04.Screen/01.Screen.py
@@ -5,7 +5,7 @@ from kivymd.uix.button import MDFlatButton
 class demo(MDApp):
     def build(self):
         scr = Screen();
-        fb = MDFlatButton(text="AR World",size_hint=(.1 , .1 ),pos=(300,300));
+        fb = MDFlatButton(text="AR World",size_hint=(.1 , .1 ),pos=(300,300),md_bg_color = (0, 1, 1 , .6));
         scr.add_widget(fb);
         return scr 
 demo().run(); 
